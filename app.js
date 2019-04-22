070e15dfb93a8fbf592f64cbfdb2b12a33c3ae97
tsolron <tsolron@gmail.com>
2019-04-22 23:27:29 +0700

    Updated for tutorial #5. Canvas div isn't working quite right, but good enough for me to move on

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ new Vue({
     name: 'Graham',
     job: 'Gamer',
     website: 'http://www.tsolron.com',
-    websiteTag: '<a href="http://www.tsolron.com">Tsolron\'s website alternate method</a>'
+    websiteTag: '<a href="http://www.tsolron.com">Tsolron\'s website alternate method</a>',
+    age: 26,
+    x: 0,
+    y: 0
   },
   methods: {
-    greet: function(time){
+    greet: function(time) {
       return 'Good ' + time + ' ' + this.name;
+    },
+    addYear: function(years) {
+      this.age += years;
+    },
+    updateXY: function(event) {
+      //console.log(event);
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
